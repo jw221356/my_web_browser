3ed3442d3c09078a94cf59c884365884aadc7e4c
jeong won <jw221356@naver.com>
2023-12-26 15:46:11 +0900

    뒤로가기 제어

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
-
-import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -22,8 +21,44 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('나만의 웹브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              _controller.loadRequest(Uri.parse(value));
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<String>(
+                value: 'https://www.google.co.kr',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.naver.com',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.kakao.com',
+                child: Text('카카오'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: WillPopScope(
+        onWillPop: () async {
+          if (await _controller.canGoBack()) {
+            await _controller.goBack();
+            return false;
+          }
+          return true;
+        },
+        child: WebViewWidget(
+          controller: _controller,
+        ),
       ),
     );
   }
 }
-
